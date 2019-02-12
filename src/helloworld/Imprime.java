7890c9cf157d0ed9ef2243626a0dd25581e876b4
159928LJonathanRodriguezMontiel <46726402+JonaRdzM@users.noreply.github.com>
2019-02-12 12:17:22 -0700

    Add files via upload

---
+++ src/helloworld/Imprime.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Imprime{
+public static void main(String[] args)
+{
+Scanner input= new Scanner (System.in); //**declaro la libreria scanner de entrada
+int a= ((-5)+8*6);
+int b= (55+9)%9;
+int c= 20+(-3*5/8);
+int d= 5+15/3*2-8%3;
+
+System.out.print(a + "\n" + b + "\n" + c + "\n" + d);
+}
+}
