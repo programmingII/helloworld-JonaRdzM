3b09a217507ac5c8d95c0238a17ac01eaebfca22
159928LJonathanRodriguezMontiel <46726402+JonaRdzM@users.noreply.github.com>
2019-02-12 12:24:31 -0700

    Update Divi.java

--- src/helloworld/Divi.java
+++ src/helloworld/Divi.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Divi{
 public static void main(String[] args)
 {
-Scanner input = new Scanner (System.in);
+Scanner input = new Scanner (System.in); //** Scanner sera de entrada de datos///**
+  
 System.out.print("Ingrese su primer numero : ");
-int numero1= input.nextInt();
-System.out.print("Ingrese su segundo numero : ");
-int numero2= input.nextInt();
+int numero1= input.nextInt(); //** se asigna el primer numero a la variable numero1//**
+System.out.print("Ingrese su segundo numero : ");  ///**Segundo numero///*
+int numero2= input.nextInt(); //** se asigna el segundo numero///**
+  
 int divi= (numero1/numero2);
 
 System.out.print("Su multiplicacion es : " +divi);
