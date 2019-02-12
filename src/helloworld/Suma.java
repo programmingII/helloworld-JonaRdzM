55e97f045c342ed6c801fd3f1902688b7e64dbc3
159928LJonathanRodriguezMontiel <46726402+JonaRdzM@users.noreply.github.com>
2019-02-12 12:29:04 -0700

    Update Suma.java

--- src/helloworld/Suma.java
+++ src/helloworld/Suma.java
@@ -4,12 +4,14 @@ public class Suma{
   public static void main(String[] args)
 {
   Scanner input = new Scanner (System.in);
-System.out.print(""Calcu de Sumas"");
-  System.out.print("Ingrese el primer Numero : ");
-  int numero1= input.nextInt();
+  System.out.print("Calcu de Sumas");
+  System.out.print("Ingrese su primer Numero : ");
+  int numero1= input.nextInt(); ///* Asignacion del primer numero a la variable numero1///**
+    
   System.out.print("Ingrese su segundo Numero : ");
-  int numero2= input.nextInt();
+  int numero2= input.nextInt();  ///** asignacion del segundo numero a la variable numero2///**
   int suma=numero1+numero2;
+ 
   System.out.println();
   System.out.println("Su suma es = "+suma);
 }
