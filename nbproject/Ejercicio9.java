c6ff744cc8950d364663f1b3fca1613dbc6ed54e
159928LJonathanRodriguezMontiel <46726402+JonaRdzM@users.noreply.github.com>
2019-02-21 13:40:53 -0600

    Update Ejercicio9.java

--- nbproject/Ejercicio9.java
+++ nbproject/Ejercicio9.java
@@ -1,7 +1,7 @@
-public class Ejercicio9
+public class Ejercicio9  ///** Clase principal con el nombre del programa///**
 {
-public static void main(String[] args)
+public static void main(String[] args) ////** Creamos el corazon del programa, el main con sus atributos///*/
 {
-System.out.println((25.5*3.5-3.5*3.5) / (40.5-4.5));
+System.out.println((25.5*3.5-3.5*3.5) / (40.5-4.5));  ///** aqui pedimos que se nos muestre por medio de un Sytem.out la formula///***
 }
 }
