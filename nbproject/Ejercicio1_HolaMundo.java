f644f88608ffb39ce15b141fdbbfa7c8077c5320
159928LJonathanRodriguezMontiel <46726402+JonaRdzM@users.noreply.github.com>
2019-02-21 10:26:57 -0600

    Update Ejercicio1_HolaMundo.java

--- nbproject/Ejercicio1_HolaMundo.java
+++ nbproject/Ejercicio1_HolaMundo.java
@@ -1,5 +1,5 @@
-class Ejercicio1_HolaMundo{
-public static void main(String[] args){
-System.out.println("Hola Mundo!");
+class Ejercicio1_HolaMundo{   ///** Creamos la clase del programa///**
+public static void main(String[] args){  ///*** aqui tenemos lo que es el alma del programa el main con sus atributos///**
+System.out.println("Hola Mundo!");  ///*** mandamos a imprimir el hola mudo con la instruccion System.out///***
 }
 }
