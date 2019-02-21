94fb077af168bda8f7246a6e3b4bab1b2df0e83e
159928LJonathanRodriguezMontiel <46726402+JonaRdzM@users.noreply.github.com>
2019-02-21 11:57:55 -0600

    Update Ejercicio10.java

--- nbproject/Ejercicio10.java
+++ nbproject/Ejercicio10.java
@@ -1,7 +1,7 @@
-public class Ejercicio10{
-public static void main(String[] args)
+public class Ejercicio10{  ///** Creamos la clase del ejercico///*
+public static void main(String[] args)///*** cremos el cuerpo del programa con su main y sus atributos y sus funciones dentro///*
 {
-double formula= 4.0*(1.0-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11));
-System.out.print(formula);
+double formula= 4.0*(1.0-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11));////* aqui escribimos la formula que nos pide el ejercicio///*
+System.out.print(formula); ////** y aqui mandamos a imprimir la formula antes declarada///**
 }
 }
