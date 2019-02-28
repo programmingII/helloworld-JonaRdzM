f0b611c3ef3ccf913508803607e494e916ac0ea6
159928LJonathanRodriguezMontiel <46726402+JonaRdzM@users.noreply.github.com>
2019-02-28 08:42:39 -0600

    Add files via upload

---
+++ nbproject/Ejercicio43.java
@@ -0,0 +1,6 @@
+public class Ejercicio43 {
+	public static void main(String[] args) {
+	
+   System.out.println("\nBrilla, brilla, peque;a estrella, \n\tComo me pregunto lo que eres! \n\t\tPor encima del mundo tan arriba, \n\t\tComo un diamente en el cielo. \nBrilla, brilla, peque;a estrella, \n\tComo me pregunto lo que eres!\n\n");
+}
+}
